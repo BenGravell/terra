7d172535e1bbeb9189a42fe15badf73006dafd8d
Ben <42848659+BenGravell@users.noreply.github.com>
2023-10-28 22:26:08 -0400

    upgrade requirements packages

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ from setuptools import setup, find_packages
 setup(
     name="terra",
     version="0.0.2",
+    python_requires=">=3.11",
     author="Benjamin Gravell",
     author_email="",
     description="Country recommendation streamlit app",
--- terra/app_config.py
+++ terra/app_config.py
@@ -1,11 +1,47 @@
 import matplotlib.pyplot as plt
 import streamlit as st
-from streamlit_extras.metric_cards import style_metric_cards
+
+# from streamlit_extras.metric_cards import style_metric_cards
 
 import terra.streamlit_config as streamlit_config
 
 
 TTL = 1 * 60 * 60  # Default time-to-live for streamlit cache, in seconds
+
+
+# TODO: Use streamlit_extras.metric_cards.style_metric_cards once support for streamlit 1.28.0 is provided.
+# NOTE: Copied & modified from streamlit_extras.metric_cards.style_metric_cards
+# To support change in streamlit 1.28.0 from 1.27.2 to the div name for st.metric.
+# (OLD) data-testid="metric-container"
+# (NEW) data-testid="stMetric"
+def style_metric_cards(
+    background_color: str = "#FFF",
+    border_size_px: int = 1,
+    border_color: str = "#CCC",
+    border_radius_px: int = 5,
+    border_left_color: str = "#9AD8E1",
+    box_shadow: bool = True,
+):
+    box_shadow_str = (
+        "box-shadow: 0 0.15rem 1.75rem 0 rgba(58, 59, 69, 0.15) !important;"
+        if box_shadow
+        else "box-shadow: none !important;"
+    )
+    st.markdown(
+        f"""
+        <style>
+            div[data-testid="stMetric"] {{
+                background-color: {background_color};
+                border: {border_size_px}px solid {border_color};
+                padding: 5% 5% 5% 10%;
+                border-radius: {border_radius_px}px;
+                border-left: 0.5rem solid {border_left_color} !important;
+                {box_shadow_str}
+            }}
+        </style>
+        """,
+        unsafe_allow_html=True,
+    )
 
 
 def streamlit_setup():
